c024dd080cec11e9d38fae0bfb0176a143148591
Bhargav Chandaka <bhargav2900@gmail.com>
2021-10-26 08:54:09 -0500

    Setup Colcon Hook to set Gazebo Paths

--- basic_sim/setup.py
+++ basic_sim/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'hook'), glob("hook/*.sh")),
         # Path to the launch file      
         (os.path.join('share', package_name,'launch'), glob('launch/*.launch.py')),
 
